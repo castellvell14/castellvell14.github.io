b69e70434945e9e9fa5bb2ee1c832ed6c466fabd
castellvell14 <castellvell14@gmail.com>
2025-03-04 12:29:11 +0100

    Improve countdown and add confetti

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "06991d7052cae55c518fe845c3395650",
+"flutter_bootstrap.js": "66423fb3b0e0b01b1e0052cd2cc1fc64",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "f628d80f4db5de7fd633d878548303cf",
 "/": "f628d80f4db5de7fd633d878548303cf",
-"main.dart.js": "428187f13ee00c4863129931535f3dfd",
+"main.dart.js": "73b41b58d4a96463f84d9b5b37b11cc6",
 "manifest.json": "e0337db998eb14e172179b1553fe03fc",
 "version.json": "40e258b2d6bf2b44244ae46f22b9cb9d"};
 // The application shell files that are downloaded before a service worker can
